32132bbcee7a363ac161ce4727bf8f9a9469e907
Collin Jilbert <cjilbert504@gmail.com>
2022-10-05 16:51:39 -0500

    fixing a mistake I made keeping the code in line with video recording :)

--- test/test_parity_prop.rb
+++ test/test_parity_prop.rb
@@ -1,4 +1,9 @@
 require "minitest/autorun"
 require_relative "../lib/parity_prop"
 
-[1, 2, 3, 4].split_by_parity
+class ParityPropTest < Minitest::Test
+  def test_split_by_parity
+    arr = [1, 2, 3, 4]
+    assert_equal arr.partition(&:even?), arr.split_by_parity
+  end
+end
